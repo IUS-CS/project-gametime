dd32f9f40eaebe5d7af849275c2344f826fda0dc
Jacob <jacolizer2019@gmail.com>
2026-02-18 02:48:02 -0500

    is the .env file pushed?

--- backend/src/restapi/gametime/views.py
+++ backend/src/restapi/gametime/views.py
@@ -1,9 +1,30 @@
-from django.shortcuts import render
+import requests
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .auth import get_access_token, get_client_id, authenticate
 
 # Create your views here.
+clientID = get_client_id()
+url = 'https://api.igdb.com/v4/games'
+
 
 @api_view(['GET'])
 def healthCheck(request):
-    return Response({"message":"The backend is connected to the button"}, status=200)
+    return Response({"message": "The backend is connected to the button"}, status=200)
+
+
+@api_view(['GET'])
+def getGames(request):
+    try:
+        access_token = get_access_token()
+        headers = {
+            "Client-ID": f"{clientID}",
+            "Authorization": f"Bearer {access_token}"
+        }
+        response = requests.post(url, headers=headers, data='search "resident evil"; fields name, summary;')
+        if response.status_code == 401:
+            access_token = authenticate()
+            response = requests.post(url, headers=headers, data='search "resident evil"; fields name, summary;')
+        return Response(response.json(), status=response.status_code)
+    except Exception as e:
+        return Response("Something went wrong", status=500)
