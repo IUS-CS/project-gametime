5ed71f17e7fbf18d3cfe4a75f650084fe9f6020d
Michael Verdouw <mverdouw@iu.edu>
2026-03-02 14:28:17 -0500

    added UserReview class

--- backend/src/restapi/Reviews/test_UserReview.py
+++
@@ -1,8 +0,0 @@
-import unittest
-import UserReview
-
-class TestUserReview(unittest.TestCase):
-
-    def test_InitReview(self):
-        user1 = UserReview(10, 12, 5, "lorem ipsum")
-        self.assertEqual(user1.gameID, 10)
--- backend/src/restapi/gametime/models.py
+++ backend/src/restapi/gametime/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
+import time
+import sqlite3
+databasePath = "filepath/database.db"
+
+class UserReview:
+    def __init__(self, gameID, userID, rating, comment):
+        self.gameID = gameID
+        self.userID = userID
+        self.rating = rating
+        self.comment = comment
+        self.date = time.time()
+
+    def getGameID(self):
+        return self.gameID
+    
+    def getUserID(self):
+        return self.userID
+    
+    def getRating(self):
+        return self.rating
+    
+    def getComment(self):
+        return self.comment
+    
+    def getDate(self):
+        return self.date
+    
+    def pushToDatabase(self):
+        return
+    
+    def pullFromDatabase(self):
+        return
+    
+    
 # Create your models here.
