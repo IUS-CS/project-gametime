d1d6427db9b8a67999401db1d71835fc8e6999f6
Jacob <jacolizer2019@gmail.com>
2026-04-06 21:57:21 -0400

    got test structure here and fix some stuff and reviews

--- backend/src/restapi/gametime/tests.py
+++ backend/src/restapi/gametime/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import USER, REVIEWS
 
 # Create your tests here.
+
+
+
+
+
+class GameTimeTest(APITestCase):
+
+    def setUp(self):
+        user = USER.objects.create_user(username='mike', email='<EMAIL>', password='password')
+        reviews = REVIEWS.objects.create(gameID = 2239, userID = user, review = "lorem ipsum dolor sit", rating = 5)
+
+
+    def test_game(self):
+        url = reverse('game', kwargs={'id': 2239})
+        response = self.client.get(url, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
